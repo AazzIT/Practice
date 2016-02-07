0a6d4f32a12b87d4c27d69a6d0f428a506118b48
Aazz <aazz@i.ua>
2016-02-07 22:52:49 +0200

    commit from notebook

--- src/practice001/matrixtraversal_008/MatrixTraversal.java
+++ src/practice001/matrixtraversal_008/MatrixTraversal.java
@@ -1,7 +1,42 @@
 package practice001.matrixtraversal_008;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MatrixTraversal {
     public static int[] print(int[][] input) {
-        return new int[]{1, 2, 3,};
+        int top = 0;
+        int bottom = input.length - 1;
+        int left = 0;
+        int right = input[0].length - 1;
+        int[] out = new int[input.length * input[0].length];
+        int i = 0;
+        while (true) {
+            // Going Right
+            if (left == right + 1) break;
+            for (int col = left; col <= right; col++) {
+                out[i++] = input[top][col];
+            }
+            top++;
+            // Going Down
+            if (top == bottom + 1) break;
+            for (int row = top; row <= bottom; row++) {
+                out[i++] = input[row][right];
+            }
+            right--;
+            // Going Left
+            if (left == right + 1) break;
+            for (int col = right; col >= left; col--) {
+                out[i++] = input[bottom][col];
+            }
+            bottom--;
+            // Going Up
+            if (top == bottom + 1) break;
+            for (int row = bottom; row >= top; row--) {
+                out[i++] = input[row][left];
+            }
+            left++;
+        }
+        return out;
     }
 }
--- src/practice001/matrixtraversal_008/Runner.java
+++ src/practice001/matrixtraversal_008/Runner.java
@@ -1,8 +1,11 @@
 package practice001.matrixtraversal_008;
 
+import java.util.Arrays;
+
 public class Runner {
     public static void main(String[] args) {
         int[][] intArray = {{1, 2, 3, }, {4, 5, 6, }, {7, 8, 9, }, {10, 11, 12, }};
-        System.out.println(MatrixTraversal.print(intArray));
+        int[] result = MatrixTraversal.print(intArray);
+        System.out.println(Arrays.toString(result));
     }
 }
