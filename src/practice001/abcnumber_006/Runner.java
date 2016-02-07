ec9e5859d5320596ef27e404900a322ff02f508e
Aazz <aazz@i.ua>
2016-02-07 19:42:39 +0200

    commit from notebook

--- src/practice001/abcnumber_006/Runner.java
+++ src/practice001/abcnumber_006/Runner.java
@@ -2,7 +2,7 @@ package practice001.abcnumber_006;
 
 public class Runner {
     public static void main(String[] args) {
-        String abcNumber = "123";
+        String abcNumber = "dbac";
         System.out.println(AbcNumber.convert(abcNumber));
     }
 }
