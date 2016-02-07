3ca6af7c9a91f39b6f0c736dd25cea8e37c6e7d7
Aazz <aazz@i.ua>
2016-02-07 15:15:12 +0200

    commit from notebook

--- src/practice001/findmaxnumber_002/Runner.java
+++ src/practice001/findmaxnumber_002/Runner.java
@@ -2,7 +2,7 @@ package practice001.findmaxnumber_002;
 
 public class Runner {
     public static void main(String[] args) {
-        int[] intArray = {10, 20, -15, -276, 267};
+        int[] intArray = {11, 3, 7 ,10, 20, -15, -276, 267};
         System.out.println(FindMaxNumber.max(intArray));
     }
 }
---
+++ src/practice001/firstoddnumber_003/Runner.java
@@ -0,0 +1,8 @@
+package practice001.firstoddnumber_003;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] intArray = {3, };
+        System.out.println(FirstOddNumber.find(intArray));
+    }
+}
