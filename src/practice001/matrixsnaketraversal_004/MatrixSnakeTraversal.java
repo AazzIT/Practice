b90b5128f964c78d00e3448b5f3ba0fa50be8967
Aazz <aazz@i.ua>
2016-02-07 18:32:11 +0200

    commit from notebook

---
+++ src/practice001/matrixsnaketraversal_004/MatrixSnakeTraversal.java
@@ -0,0 +1,24 @@
+package practice001.matrixsnaketraversal_004;
+
+public class MatrixSnakeTraversal {
+    public static int[] print(int[][] input) {
+        int[] ret = new int[input.length * input[0].length];
+        System.out.println(ret.length);
+        System.out.println(input.length);
+        System.out.println(input[0].length);
+        int k = 0;
+        for (int i = 0; i < input.length; i++) {
+
+                for (int j = 0; j < input[i].length; j++) {
+                    if ((i % 2) == 0) {
+                        ret[k + j] = input[j][i];
+                    } else {
+                        ret[k + j] = input[input[i].length - (j + 1)][i];
+                    }
+                    System.out.println(ret[k + j]);
+                }
+            k += input[i].length;
+        }
+        return ret;
+    }
+}
