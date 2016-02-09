459134064cb717296debf2c59055f0a041c4a577
Aazz <aazz@i.ua>
2016-02-09 07:25:30 +0200

    commit from notebook

--- src/practice001/matrixsnaketraversal_004/MatrixSnakeTraversal.java
+++ src/practice001/matrixsnaketraversal_004/MatrixSnakeTraversal.java
@@ -2,18 +2,18 @@ package practice001.matrixsnaketraversal_004;
 
 public class MatrixSnakeTraversal {
     public static int[] print(int[][] input) {
-        int[] ret = new int[input.length * input[0].length];
+        int[] resultArray = new int[input.length * input[0].length];
         int rowsCount = input.length;
         int colsCount = input[0].length;
         for (int col = 0; col < colsCount; col++) {
                 for (int row = 0; row < rowsCount; row++) {
                     if ((col % 2) == 0) {
-                        ret[col * rowsCount + row] = input[row][col];
+                        resultArray[col * rowsCount + row] = input[row][col];
                     } else {
-                        ret[col * rowsCount + row] = input[rowsCount - row - 1][col];
+                        resultArray[col * rowsCount + row] = input[rowsCount - row - 1][col];
                     }
                 }
         }
-        return ret;
+        return resultArray;
     }
 }
--- src/practice001/sumdigits_001/SumDigits.java
+++ src/practice001/sumdigits_001/SumDigits.java
@@ -4,13 +4,10 @@ public class SumDigits {
     public static int sum(int number) {
         int result = 0;
         String temp = ((Integer) number).toString().replace("-", "");
-        System.out.println(temp);
-
         for (int i = 0; i < temp.length(); i++) {
             result += ((int) temp.charAt(i) - 48);
 
         }
-
         return result;
     }
 }
