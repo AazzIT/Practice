80e5a6ef55c002508fc96b9f936e211a4d8e5b73
Aazz <aazz@i.ua>
2016-02-09 07:27:18 +0200

    commit from notebook

--- src/practice001/matrixtraversal_008/MatrixTraversal.java
+++ src/practice001/matrixtraversal_008/MatrixTraversal.java
@@ -6,34 +6,34 @@ public class MatrixTraversal {
         int bottom = input.length - 1;
         int left = 0;
         int right = input[0].length - 1;
-        int[] out = new int[input.length * input[0].length];
+        int[] resultArray = new int[input.length * input[0].length];
         int i = 0;
         while (true) {
             // Going Right
             if (left == right + 1) break;
             for (int col = left; col <= right; col++) {
-                out[i++] = input[top][col];
+                resultArray[i++] = input[top][col];
             }
             top++;
             // Going Down
             if (top == bottom + 1) break;
             for (int row = top; row <= bottom; row++) {
-                out[i++] = input[row][right];
+                resultArray[i++] = input[row][right];
             }
             right--;
             // Going Left
             if (left == right + 1) break;
             for (int col = right; col >= left; col--) {
-                out[i++] = input[bottom][col];
+                resultArray[i++] = input[bottom][col];
             }
             bottom--;
             // Going Up
             if (top == bottom + 1) break;
             for (int row = bottom; row >= top; row--) {
-                out[i++] = input[row][left];
+                resultArray[i++] = input[row][left];
             }
             left++;
         }
-        return out;
+        return resultArray;
     }
 }
